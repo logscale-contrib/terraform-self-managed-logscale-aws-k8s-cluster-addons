fb19743716079223e3bf0fd7fc9e19393ef56d7f
Ryan Faircloth <ryan@dss-i.com>
2022-12-10 09:42:03 -0500

    feat: add external-dns

--- variables.tf
+++ variables.tf
@@ -45,3 +45,8 @@ variable "region" {
   type = string
   description = "(optional) describe your variable"
 }
+
+variable "zone_id" {
+  type = string
+  description = "(optional) describe your variable"
+}
